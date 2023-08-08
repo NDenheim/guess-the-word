990e735fb3f957c20ed22a3c77d9c7636702361c
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-08 12:40:03 +0100

    Initial functionality - QS for relevant elements

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
 import "./style.scss";
+import { Word, wordArray } from "./types/Words";
+
+const answerDisplay = document.querySelector(".answer") as HTMLDivElement;
+const categoryMenu = document.querySelector(
+  "#category-menu"
+) as HTMLSelectElement;
+const difficultyMenu = document.querySelector(
+  "#difficulty-menu"
+) as HTMLSelectElement;
+const alphabetButtons = document.querySelectorAll(".letters__button");
+
+if (!answerDisplay || !categoryMenu || !difficultyMenu || !alphabetButtons) {
+  throw new Error("Issue with QS");
+}
+
+// const easyCities = [
+//   "london",
+//   "paris",
+//   "madrid",
+//   "sydney",
+//   "amsterdam",
+//   "tokyo",
+// ];
+
+if (categoryMenu.innerText == "Cities" && difficultyMenu.innerText == "Easy") {
+  const words = easyCities;
+  console.log(words);
+}
+// const regularCities = [
+//   "prague",
+//   "venice",
+//   "vienna",
+//   "istanbul",
+//   "bangkok",
+//   "milan",
+// ];
+// const hardCities = [];
+
+const handleMenuSelect = () => {};
